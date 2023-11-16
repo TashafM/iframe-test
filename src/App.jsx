23f1b62c135f194d1a99e190f84f878cd98837d2
TashafMahmood <tashaf@elred.io>
2023-11-16 11:54:44 +0530

    sdfghjk

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="mailto:mahmood@gmail.com">send email to tashaf</a><br />
+      <a href="https://www.facebook.com">open facebook</a><br />
+    </div>
+  );
+}
+
+export default App;
